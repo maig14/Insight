c4e1ee9a06700a536ecc1485b15ac81222b717ce
Louise Giam <maig14@users.noreply.github.com>
2020-02-10 09:07:53 -0800

    app.py depends on model.pkl
    new model.pkl depends on results, study type, funded_by

--- app.py
+++ app.py
@@ -2,8 +2,40 @@ import joblib
 import pickle
 import numpy as np
 import pandas as pd
+
+# https://towardsdatascience.com/how-to-write-web-apps-using-simple-python-for-data-scientists-a227a1a01582
+# st.slider
+
 import streamlit as st
 import os
+
+# Sample smiles for users to predict on
+#example_diseases = [
+#    '',
+#    'Bladder',
+#    'Bone',
+#    'Brain',
+#    'Breast',
+#    'Cervical',
+#    'Colorectal',
+#    'Esophageal',
+#    'Gastric',
+#    'Head and Neck',
+#    'Kidney',
+#    'Leukemias',
+#    'Liver',
+#    'Lung',
+#    'Lymphomas',
+#    'Melanoma',
+#    'Multiple Myeloma',
+#    'Ovarian',
+#    'Pancreatic',
+#    'Prostate',
+#    'Soft-tissue',
+#    'Testicular',
+#    'Thyroid',
+#    'Uterine'
+#]
 
 example_diseases = [
     '',
@@ -15,46 +47,83 @@ example_diseases = [
     'Multiple Myeloma',
 ]
 
-phases = [
+phase = [
     '',
     'Phase 1',
     'Phase 2',
     'Phase 3'
 ]
 
-results = [
+study_results = [
     '',
-    'Yes',
-    'No',
+    'Has Results',
+    'No Results Available'
 ]
 
-example_sites = [
+study_type = [
     '',
-    'MD Anderson Cancer Center',
-    'National Cancer Institute',
-    'Memorial Sloan Kettering Cancer Center',
-    'Roche',
-    'Novartis',
-    'Mayo Clinic'
+    'Interventional',
+    'Observational'
+]
+
+funded_by = [
+    '',
+    'National Institute of Health',
+    'Industry',
+    'Other',
+    'U.S. Fed'
 ]
 
 st.markdown('#### [Insight Data Science Fellowship](https://www.insightdatascience.com/)')
 st.markdown('#### Made by [Louise Giam](https://www.linkedin.com/in/louise-giam/)')
-st.markdown('#### Feb 2020 Health Data Science')
+st.markdown('#### 2020A Health Data Science')
 
-st.markdown('## Assessing Clinical Trial Completion')
+st.markdown('## Clinical Trial Insights')
 st.markdown('---')
 
-disease = st.selectbox('Cancer', example_diseases)
-phase = st.selectbox('Phase', phases)
-company = st.selectbox('Company', example_sites)
-enroll = st.number_input('Enrollment')
-results = st.selectbox('Has Results?', results)
+# model = pickle.load(open('model.pkl', 'rb'))  # get the model
 
-features = [enroll, phases, results]
+# experience = st.number_input('Disease/Condition')
+# phase = st.number_input('Phase')
+#enroll = st.number_input('Enrollment')
+#disease = st.selectbox('Disease', example_diseases)    
 
-# model = joblib.load(open('model.pkl', 'rb'))
+# phase = st.selectbox('Phase', phase)
+sr = st.selectbox('Results?', study_results)
+stype = st.selectbox('Study Type?', study_type)
+fb_list = st.multiselect('Funded By?', funded_by)
+
+# returns a list
+
+# def file_selector(folder_path='.'):
+#    filenames = os.listdir(folder_path)
+#    selected_filename = st.selectbox('Select a file', filenames)
+#    return os.path.join(folder_path, selected_filename)
+
+# filename = file_selector()
+# st.write('You selected `%s`' % filename)
+sr = [sr]
+stype = [stype]
+z = []
+fb_nih = fb_list.count('NIH')
+fb_ind = fb_list.count('Industry')
+fb_oth = fb_list.count('Other')
+fb_fed = fb_list.count('U.S. Fed')
+z.append([fb_nih, fb_ind, fb_oth, fb_fed])
+z_df = pd.DataFrame(z, columns = ['NIH', 'Industry', 'Other', 'U.S. Fed'])
+
+test2_df = pd.DataFrame({'Study Results': sr, 'Study Types': stype})
+
+test3 = test2_df.join(z_df)
+
+# int_features = [int(x) for x in features]		# fix
+# final_features = [np.array(int_features)]		# fix
+# X_test = pd.read_csv(filename)
+
+m = pickle.load(open('model.pkl', 'rb'))
+prediction = m.predict(test3)[0]
+prob = m.predict_proba(test3)[0,0]
 
 if st.button('Predict'):
-	st.success(f'Likelihood of completion: 63%')
-#	st.success(f'Your Salary per anum is: Ghc {round(prediction[0], 2)}')
+    st.success("This trial will be: " + m.predict(test3)[0])
+    st.success("The likelihood is: " + "{:,.2%}".format(prob))
